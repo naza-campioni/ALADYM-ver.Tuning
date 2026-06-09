764450b5992d8dc39a682dffb8634739fffdda47
naza-campioni <56881474+naza-campioni@users.noreply.github.com>
2026-06-09 11:56:31 +0200

    Update create_sel_combinations.R

--- Utils/Selectivity_tuning/ALADYM_tuning_selectivity_utils/create_sel_combinations.R
+++ Utils/Selectivity_tuning/ALADYM_tuning_selectivity_utils/create_sel_combinations.R
@@ -35,93 +35,6 @@ create_combinations <- function(folder, params_sel, selectivity_id, spe_folder, 
                   header = TRUE)
   sel[is.na(sel)] = ""
   
-  split_gear <- function(df, name_gear){
-    
-    return(df[df$fleet_segment == name_gear, ])
-    
-  }
-  
-  df_by_gears <- list()
-  
-  for (names in unique(sel$fleet_segment)) {
-    df_by_gears <- append(df_by_gears, list(split_gear(sel, names)))
-  }
-  
-  
-  
-  # create combo per fleet
-  create_combo <- function(df){
-    
-    sel_type             <- paste(df$sel_type[1])
-    names                <- df$fleet_segment[1]
-    param_names          <- selectivity_id[[sel_type]][[1]]
-    # id                   <- selectivity_id[[sel_type]][[2]]
-    params               <- params_sel[[sel_type]] 
-    
-    combo_grid           <- expand.grid(params)
-    colnames(combo_grid) <- param_names
-    
-    
-    return(list(combo_grid, names)) 
-  }
-  
-  # 
-  fleet_config <- list()
-  fleet_config_name <- list()
-  
-  for (i in 1:length(df_by_gears)) {
-    output            <- create_combo(df_by_gears[[i]])
-    fleet_config      <- append(fleet_config, list(output[[1]]))
-    fleet_config_name <- append(fleet_config_name, list(output[[2]]))
-  }
-  
-  names(fleet_config) <- fleet_config_name
-  
-  
-  # create unique  param names
-  fleet_config <- Map(function(df, nm) {
-    names(df) <- paste(nm, names(df), sep = "_")
-    df
-  }, fleet_config, names(fleet_config))
-  
-  
-  # generate all global configurations
-  full_configs <- Reduce(function(x, y) merge(x, y, by = NULL), fleet_config)
-  
-  
-  build_selectivity_config <- function(template, config_row, gear_param_map) {
-    
-    df <- template
-    
-    # overwrite parameters for each gear
-    for (gear in names(gear_param_map)) {
-      
-      cols <- gear_param_map[[gear]]             # columns in full_configs belonging to this gear
-      params <- config_row[cols]                 # the actual values for this config
-      
-      # subset of template belonging to this gear
-      rows <- df$fleet_segment == gear
-      
-      # find which param indexes are relevant depending on sel_type
-      sel_type <- df$sel_type[rows][1]
-      relevant_params <- selectivity_id[[as.character(sel_type)]][[2]]
-      
-      # assign relevant params
-      for (i in seq_along(relevant_params)) {
-        param_col <- paste0("param", relevant_params[i])
-        df[rows, param_col] <- params[i]
-      }
-    }
-    
-    return(df)
-  }
-  
-  gear_param_map <- lapply(names(fleet_config), function(g) {
-    grep(paste0("^", g, "_"), names(full_configs), value = TRUE)
-  })
-  names(gear_param_map) <- names(fleet_config)
-  
-  template <- sel   # your original full selectivity table
   
   dir_to_create <- "selectivity_combinations"
   
@@ -130,22 +43,71 @@ create_combinations <- function(folder, params_sel, selectivity_id, spe_folder, 
   }
   
   
-  for (i in 1:nrow(full_configs)) {
+  fleet_config <- list()
+  
+  for (type in unique(sel$type)) {
     
-    cfg_row <- full_configs[i, ]
+    params <- params_sel[[type]]
     
-    df_cfg <- build_selectivity_config(
-      template = template,
-      config_row = cfg_row,
-      gear_param_map = gear_param_map
-    )
+    combo_grid <- expand.grid(params)
     
-    outfile <-  sprintf("selectivity_cfg_%04d.csv", i)
-    path <- paste(folder, "/", spe_folder, "/selectivity_combinations/", outfile, sep='')
-    write.csv2(df_cfg, path, row.names = FALSE)
+    colnames(combo_grid) <- selectivity_id[[type]][[1]]
+    
+    fleet_config[[type]] <- combo_grid
   }
   
   
+  # -----------------------------
+  # CREATE FILES
+  # -----------------------------
+  
+  cfg_id <- 1
+  
+  for (type in names(fleet_config)) {
+    
+    configs <- fleet_config[[type]]
+    
+    for (i in 1:nrow(configs)) {
+      
+      # start from original file
+      df_cfg <- sel
+      
+      # rows belonging to current type
+      idx <- df_cfg$type == type
+      
+      # apply parameter values
+      for (param_name in colnames(configs)) {
+        
+        df_cfg[idx, param_name] <- configs[i, param_name]
+      }
+      
+      # output filename
+      outfile <- sprintf(
+        "selectivity_cfg_%04d.csv",
+        cfg_id
+      )
+      
+      path <- paste(
+        folder,
+        "/",
+        spe_folder,
+        "/selectivity_combinations/",
+        outfile,
+        sep = ""
+      )
+      
+      # save
+      write.table(
+        df_cfg,
+        path,
+        sep = ';',
+        row.names = FALSE,
+        quote = FALSE
+      )
+      
+      cfg_id <- cfg_id + 1
+    }
+  }
   
 } 
 
